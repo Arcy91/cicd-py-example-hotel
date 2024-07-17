445dd02130e298820b9516737d9dd39ed33c033d
Arcy91 <90647620+Arcy91@users.noreply.github.com>
2024-07-17 19:21:23 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -15,6 +15,7 @@ class TestHootel(object):
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
         self.browser.maximize_window()
+        self.browser.set_window_size(1024, 800)
 
     def teardown_method(self):
         self.browser.quit()
@@ -24,8 +25,8 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self):
-        menu_toggle = self.browser.find_element(By.XPATH,'//[@class="navbar-toggler collapsed"]')
-        menu_toggle.click()
+        #menu_toggle = self.browser.find_element(By.XPATH,'//[@class="navbar-toggler collapsed"]')
+        #menu_toggle.click()
         login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
         login_btn.click()
 
